48eec2d0c664ac346440edc4631240a5be72f62f
Vamsi Krishna <vamsi@Ranjithas-MacBook-Air.local>
2024-10-19 15:51:08 -0400

    socket setup server

--- server/index.js
+++ server/index.js
@@ -4,9 +4,21 @@ const http = require("http");
 
 const PORT = process.env.PORT || 4000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", function (socket) {
+  console.log(' we have comms"');
+
+  socket.on("disconnect", function () {
+    console.log("user left");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, function () {
   console.log(`server running in PORT ${PORT}`);
